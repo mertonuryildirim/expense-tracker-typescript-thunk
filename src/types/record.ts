dbcad82478fa7dc15039c19c298cc73830a2182f
Mert Onur YILDIRIM <mert.yildirim@aartimuhendislik.com>
2021-09-21 16:19:11 +0300

    added records types & actions & reducers

---
+++ src/types/record.ts
@@ -0,0 +1,37 @@
+import { ThunkDispatch } from "redux-thunk";
+import { Category } from "./category";
+
+export interface Record {
+  id: number;
+  title: string;
+  amount: number;
+  createdAt: string;
+  updatedAt: string;
+  category: Category;
+}
+
+export interface RecordState {
+  data: Record[];
+  loading: boolean;
+  error: string;
+}
+
+interface GET_RECORDS_START {
+  type: "GET_RECORDS_START";
+}
+
+interface GET_RECORDS_SUCCESS {
+  type: "GET_RECORDS_SUCCESS";
+  payload: Record[];
+}
+
+interface GET_RECORDS_ERROR {
+  type: "GET_RECORDS_ERROR";
+}
+
+export type RecordDispatch = ThunkDispatch<RecordState, void, RecordAction>;
+
+export type RecordAction =
+  | GET_RECORDS_START
+  | GET_RECORDS_SUCCESS
+  | GET_RECORDS_ERROR;
