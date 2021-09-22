6732350a33bb13f9f7a2a191f9f696d96d0378ac
Mert Onur YILDIRIM <mert.yildirim@aartimuhendislik.com>
2021-09-22 14:48:58 +0300

    fixed missing token issue

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -23,7 +23,7 @@ function SignUp() {
 
   const onFinish = async (values: any) => {
     try {
-      await api.post("/users/register", values);
+      await api().post("/users/register", values);
       history.push("/login", { newSignUp: true });
     } catch (error) {
       showError((error as any).response.data.errorMessage);
--- src/store/actions/recordActions.ts
+++ src/store/actions/recordActions.ts
@@ -4,7 +4,7 @@ import api from "../../utils/api";
 export const getRecords = () => async (dispatch: RecordDispatch) => {
   dispatch({ type: "GET_RECORDS_START" });
   try {
-    const response = await api.get<Record[]>("/records");
+    const response = await api().get<Record[]>("/records");
     dispatch({ type: "GET_RECORDS_SUCCESS", payload: response.data });
   } catch {
     dispatch({ type: "GET_RECORDS_ERROR" });
@@ -15,7 +15,7 @@ export const addRecord =
   (form: RecordForm) => async (dispatch: RecordDispatch) => {
     dispatch({ type: "ADD_RECORD_START" });
     try {
-      const response = await api.post<Record>("/records", form);
+      const response = await api().post<Record>("/records", form);
       dispatch({ type: "ADD_RECORD_SUCCESS", payload: response.data });
     } catch {
       dispatch({ type: "ADD_RECORD_ERROR" });
@@ -26,7 +26,7 @@ export const updateRecord =
   (form: RecordForm, id: Record["id"]) => async (dispatch: RecordDispatch) => {
     dispatch({ type: "UPDATE_RECORD_START" });
     try {
-      const response = await api.put<Record>("/records/" + id, form);
+      const response = await api().put<Record>("/records/" + id, form);
       dispatch({ type: "UPDATE_RECORD_SUCCESS", payload: response.data });
     } catch {
       dispatch({ type: "UPDATE_RECORD_ERROR" });
@@ -37,7 +37,7 @@ export const deleteRecord =
   (id: Record["id"]) => async (dispatch: RecordDispatch) => {
     dispatch({ type: "DELETE_RECORD_START" });
     try {
-      await api.delete("/records/" + id);
+      await api().delete("/records/" + id);
       dispatch({ type: "DELETE_RECORD_SUCCESS", payload: id });
     } catch {
       dispatch({ type: "DELETE_RECORD_ERROR" });
